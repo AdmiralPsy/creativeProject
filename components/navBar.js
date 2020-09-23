3c758f15a634f7b0ed897989523a82768f567f44
Zachary Gallafent <zachary@compguy.com>
2020-09-23 10:12:42 -0600

    Add navbar element

---
+++ components/navBar.js
@@ -0,0 +1,29 @@
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+        // this.root = this.attachShadow({mode: 'open'});
+        this.innerHTML = this.COMPONENT_HTML;
+    }
+
+    get COMPONENT_HTML() {
+        return `
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="navbar-brand" href="/index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Log Out</a>
+            </li>    
+        </ul>
+    </div>
+</nav>`
+    }
+}
+
+customElements.define('nav-bar', NavBar)
