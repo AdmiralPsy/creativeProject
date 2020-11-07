66fc7bed51a9093e4b88dc20e062fb9d714bc90b
Zachary Gallafent <zachary@compguy.com>
2020-11-06 23:12:05 -0700

    Update contacts

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ class Footer extends HTMLElement {
 
     get COMPONENT_HTML() {
         return `
-<span id="footer-credit">Website created by Spencer Magnusson and Zachary Gallafent. The Github is <a href="https://github.com/AdmiralPsy/creativeProject">here</a>.</span>
+<span id="footer-credit">Website created by Zachary Gallafent. The Github is <a href="https://github.com/AdmiralPsy/creativeProject">here</a>.</span>
 <span>The pages on this website are the <a href="/html/login.html">login</a> page, the <a href="/html/home.html">home</a> page, the <a href="/html/newCategory.html">new category</a> page, and the <a href="/html/contact.html">contact</a> page.</span>`
     }
 }
